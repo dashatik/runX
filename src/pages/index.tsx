71451f51dfa769560643632d5ecec5538d49c495
dasha <daria.tikhomirova1@gmail.com>
2025-04-27 04:18:23 +0300

    NOW

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,4 @@
-import Head from "next/head";
 import HeroSection from "@/components/HeroSection";
-import ProblemSection from "@/components/ProblemSection";
-import SolutionSection from "@/components/SolutionSection";
-import HowItWorksSection from "@/components/HowItWorksSection";
-import GamificationSection from "@/components/GamificationSection";
-import SocialProofSection from "@/components/SocialProofSection";
-import CallToActionSection from "@/components/CallToActionSection";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
@@ -18,7 +11,6 @@ export default function Home() {
       <Header />
       <main>
         <HeroSection />
-
       </main>
       <Footer />
     </>
